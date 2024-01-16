74ca494db09a153416369021af5516e52e06dc60
drzezga <19654689+drzezga@users.noreply.github.com>
2024-01-16 23:47:54 +0100

    Added registering functionality

---
+++ client/Assets/MainMenu/Handlers/GameStartMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using ServerConnection.Messages;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace MainMenu.Handlers
+{
+    public class GameStartMessageHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private MessageReceiverSO messageReceiverSo;
+
+        private void OnEnable()
+        {
+            messageReceiverSo.OnGameStartResponseMessage += HandleMessage;
+        }
+
+        private void OnDisable()
+        {
+            messageReceiverSo.OnGameStartResponseMessage -= HandleMessage;
+        }
+
+        private void HandleMessage(GameStartResponseMessage message)
+        {
+            
+        }
+
+    }
+}
---
+++ client/Assets/MainMenu/Handlers/ReplayListMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿using ServerConnection.Messages;
+using UnityEngine;
+
+namespace MainMenu.Handlers
+{
+    public class ReplayListMessageHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private MessageReceiverSO messageReceiverSo;
+
+        private void OnEnable()
+        {
+            messageReceiverSo.OnReplayListResponseMessage += HandleMessage;
+        }
+
+        private void OnDisable()
+        {
+            messageReceiverSo.OnReplayListResponseMessage -= HandleMessage;
+        }
+
+        private void HandleMessage(ReplayListResponseMessage message)
+        {
+            
+        }
+    }
+}
