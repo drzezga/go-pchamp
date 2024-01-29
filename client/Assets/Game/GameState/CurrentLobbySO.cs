9287b22c24d06d7a7e1fd70a41e3e0550b980f64
drzezga <19654689+drzezga@users.noreply.github.com>
2024-01-29 17:21:16 +0100

    Added lobby UI

--- client/Assets/Game/GameState/CurrentLobbySO.cs
+++ client/Assets/Game/GameState/CurrentLobbySO.cs
@@ -1,7 +1,9 @@
 ﻿using ServerConnection.Messages;
+using UnityEngine;
 
 namespace Game.GameState
 {
+    [CreateAssetMenu(fileName = "Data", menuName = "GO/CurrentLobbySO", order = 1)]
     public class CurrentLobbySO : SingleValueSO<LobbyDetails>
     { }
 }
